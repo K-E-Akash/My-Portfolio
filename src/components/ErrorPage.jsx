2c0a514e9dc30311085683e6c8aa376e458606cd
K-E-Akash <keakash1991@gmail.com>
2024-03-03 12:27:00 +0530

    router

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center flex-col gap-3'>
+        <h1>404 Error</h1>
+        <h3>Return to HomePage</h3>
+    </div>
+  )
+}
+
+export default ErrorPage

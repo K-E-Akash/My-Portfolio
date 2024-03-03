b22b3bfcec3213394a3dfa71a752573b4ad2d630
K-E-Akash <keakash1991@gmail.com>
2024-03-03 12:29:24 +0530

    Error apge

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
     <div className='w-full h-screen flex justify-center items-center flex-col gap-3'>
-        <h1>404 Error</h1>
-        <h3>Return to HomePage</h3>
+        <h1 className='text-3xl font-bold'>404 Error</h1>
+        <Link to='/'><h3 className='text-lg underline'>Return to HomePage</h3></Link>
     </div>
   )
 }
